33a8834c0816b12cfa988789dbfdb3dc06e53ad5
Sudeepto Bose <sudeeptobose23@gmail.com>
2021-09-26 11:57:21 +0600

    making comments

--- src/App.js
+++ src/App.js
@@ -26,12 +26,26 @@ function App() {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search/>
 
       <hr />
+      <List/>
+    </div>
+  );
+}
 
-      <ul>
+function Search () {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
         {list.map(function (item){
           return (
           <li key={item.objectID}>
@@ -45,7 +59,6 @@ function App() {
           );
         })}
       </ul>
-    </div>
   );
 }
 
