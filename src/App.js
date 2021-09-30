301e6a74b4e255d7d26c6d5d1eb57feae5b0db2a
Sudeepto Bose <sudeeptobose23@gmail.com>
2021-09-30 16:01:26 +0600

    callback functions

--- src/App.js
+++ src/App.js
@@ -1,49 +1,71 @@
 import './App.css';
+import * as React from 'react';
 
-const list = [
-  {
-    title : 'React',
-    url : 'https://reactjs.org/',
-    author : 'Jordan Walke',
-    num_comments : 3,
-    points : 4,
-    objectID : 0, 
-  },
-  {
-    title : 'Redux',
-    url : 'https://redux.js.org/',
-    author : 'Dan Abramov, Andrew Clark',
-    num_comments : 2,
-    points : 5,
-    objectID : 1,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title : 'React',
+      url : 'https://reactjs.org/',
+      author : 'Jordan Walke',
+      num_comments : 3,
+      points : 4,
+      objectID : 0, 
+    },
+    {
+      title : 'Redux',
+      url : 'https://redux.js.org/',
+      author : 'Dan Abramov, Andrew Clark',
+      num_comments : 2,
+      points : 5,
+      objectID : 1,
+    },
+  ];
 
-const App = () => (
+  const [searchTerm, setSearchTerm] = React.useState('');
+  
+  const handleSearch = (event) => {
+   
+    setSearchTerm(event.target.value);
+  };
+  const searchedStories = stories.filter((story) =>
+  story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return(
     <div>
       <h1>My Hacker Stories</h1>
-      <Search/>
+      <Search onSearch={handleSearch}/>
       <hr />
-      <List/>
+      <List list={searchedStories} />
     </div>
-)
+  );
+};
 
-const Search = () => {
-  // Do something in between
+const Search = (props) => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
   const handleChange = (event) => {
-    console.log(event.target.value)
-  };
+    setSearchTerm(event.target.value);
+
+    // B
+    props.onSearch(event);
+  }
+
   return(
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange}/>
+      <input id="search" type="text" onChange={props.onSearch} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
     </div>
   );
-}
+};
 
-const List = () => (
+const List = (props) => (
     <ul>
-        {list.map((item) =>(
+        {props.list.map((item) =>(
           <li key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
