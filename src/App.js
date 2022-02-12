aa0485de8e3a92cf3858c3708288810ed8fd568e
Sudeepto Bose <sudeeptobose23@gmail.com>
2022-02-12 17:32:14 +0600

    making changes

--- src/App.js
+++ src/App.js
@@ -1,81 +1,79 @@
-import './App.css';
 import * as React from 'react';
 
 const App = () => {
   const stories = [
     {
-      title : 'React',
-      url : 'https://reactjs.org/',
-      author : 'Jordan Walke',
-      num_comments : 3,
-      points : 4,
-      objectID : 0, 
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Sudeepto Bose',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
     },
     {
-      title : 'Redux',
-      url : 'https://redux.js.org/',
-      author : 'Dan Abramov, Andrew Clark',
-      num_comments : 2,
-      points : 5,
-      objectID : 1,
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('');
-  
+  const [searchTerm, setSearchTerm] = React.useState(localStorage.getItem('search') || 'React');
+
   const handleSearch = (event) => {
-   
     setSearchTerm(event.target.value);
+
+    localStorage.setItem('search', event.target.value);
   };
+
   const searchedStories = stories.filter((story) =>
-  story.title.toLowerCase().includes(searchTerm.toLowerCase())
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  return(
+  return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search onSearch={handleSearch}/>
+
+      <Search search={searchTerm} onSearch={handleSearch} />
+
       <hr />
+
       <List list={searchedStories} />
     </div>
   );
 };
 
-const Search = (props) => {
-  const [searchTerm, setSearchTerm] = React.useState('');
+const Search = ({ search, onSearch }) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input
+      id="search"
+      type="text"
+      value={search}
+      onChange={onSearch}
+    />
+  </div>
+);
 
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
 
-    // B
-    props.onSearch(event);
-  }
-
-  return(
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={props.onSearch} />
-
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
-    </div>
-  );
-};
-
-const List = (props) => (
-    <ul>
-        {props.list.map((item) =>(
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        ))}
-      </ul>
-)
+const Item = ({ item }) => (
+  <li>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </li>
+);
 
 export default App;
